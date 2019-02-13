bdd7753bb7efb601ddcfd43267801d21f027dbd8
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-02-13 09:15:01 +0100

    Add project files.

---
+++ ConsolePenns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolePenns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
